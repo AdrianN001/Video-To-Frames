a7ab7dda57c58616720c976fc67d40cf54b83cda
adrian <gogosadrian@outlook.com>
2022-10-14 19:21:46 -0400

    done

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ vidcap = cv2.VideoCapture(arguments[1])
 
 # Create a folder/dir to hold the frames
 try:
-    os.mkdir(f"./FRAMES_{arguments[1]}")
+    os.mkdir(f"./FRAMES_{arguments[1].split('.')[0]}")
 except FileExistsError:
-    pass
+    raise Exception(
+        "There's a folder for that video already. If you wish to create another one, remove it.")
+
 
 success, image = vidcap.read()
 count = 0
 
 while success:
-    # save frame as JPEG file
 
     if count % steps == 0:
-        cv2.imwrite(f"./FRAMES_{arguments[1]}/frame_#{count}.jpg", image)
+        # save frame as JPEG file
+        cv2.imwrite(
+            f"./FRAMES_{arguments[1].split('.')[0]}/frame_#{count}.jpg", image)
         success, image = vidcap.read()
         print('Read a new frame: ', success)
 
     count += 1
+print(f"Succesfully created {count // steps} image (:")
